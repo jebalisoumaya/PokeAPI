d17f6125ec9c4585fbd1df66224301d87dc76e19
jebalisoumaya <soumaya.jebali99@gmail.com>
2024-10-17 12:36:37 +0200

    3 requests

--- app.py
+++ app.py
@@ -53,6 +53,39 @@ def compare_pokemons():
         })
     return jsonify({"error": "Un ou les deux Pokémon n'ont pas été trouvés"}), 404
 
+@app.route('/type/<type_name>', methods=['GET'])
+def get_pokemon_by_type(type_name):
+    url = f'https://pokeapi.co/api/v2/type/{type_name}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        pokemon_count = len(data['pokemon'])
+        total_hp = 0
+        for p in data['pokemon']:
+            pokemon_url = p['pokemon']['url']
+            pokemon_response = requests.get(pokemon_url)
+            if pokemon_response.status_code == 200:
+                pokemon_data = pokemon_response.json()
+                total_hp += pokemon_data['stats'][0]['base_stat']
+        
+        average_hp = total_hp / pokemon_count if pokemon_count > 0 else 0
+        
+        
+        effectiveness = {
+            "double_damage_to": [t['name'] for t in data['damage_relations']['double_damage_to']],
+            "half_damage_to": [t['name'] for t in data['damage_relations']['half_damage_to']],
+            "no_damage_to": [t['name'] for t in data['damage_relations']['no_damage_to']]
+        }
+
+        return jsonify({
+            "type": type_name,
+            "pokemon_count": pokemon_count,
+            "average_hp": average_hp,
+            "effectiveness": effectiveness
+        })
+    else:
+        return jsonify({"error": "Type non trouvé"}), 404
+
 
 
 if __name__ == '__main__':
